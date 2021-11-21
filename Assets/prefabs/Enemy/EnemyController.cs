ac5a15264c46b61ef1355cebfae9751d3a8599da
jvolcy <jerry.volcy@gmail.com>
2021-11-21 14:56:25 -0500

    Step 2

--- Assets/prefabs/Enemy/EnemyController.cs
+++ Assets/prefabs/Enemy/EnemyController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        //find the player.  This should be the only GameObject tagged as 'Player'
+        try
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+        catch (System.Exception e)
+        {
+            //No object with the tag 'Player' was found
+            Debug.Log(e.ToString() + "\nNo 'Player' object was found.  Did you forget to tag your player as 'Player'?");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player != null)
+        {
+            //rotate ourself to face the player
+            transform.LookAt(player.transform);
+        }
     }
 }
