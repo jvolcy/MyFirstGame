d44365cca935b1e08f0d24b1aedbbb6d03a60da8
jvolcy <jerry.volcy@gmail.com>
2021-11-22 13:24:18 -0500

    Step 13

--- Assets/prefabs/Enemy/EnemyController.cs
+++ Assets/prefabs/Enemy/EnemyController.cs
@@ -6,10 +6,10 @@ public class EnemyController : MonoBehaviour
 {
 
     public float speed = 1f;
-    public AudioClip ghostSoundClip;
-    public AudioClip ghostHitClip;
+    public AudioClip enemySoundClip;
+    public AudioClip enemyHitClip;
     public float audioRange = 20f;
-    public float ghostFollowRange = 30f;   //ghosts will not follow unless you are within range
+    public float enemyFollowRange = 30f;   //enemys will not follow unless you are within range
 
     GameObject player;
 
@@ -17,11 +17,16 @@ public class EnemyController : MonoBehaviour
 
     AudioSource audioSource;
 
+    //enemy state ENUM
+    enum EnemyState { IDLE, ACTIVE, DYING, DEAD };
+    private EnemyState enemyState;
+
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-        audioSource.clip = ghostSoundClip;
+        audioSource.clip = enemySoundClip;
         audioSource.loop = true;
         audioSource.volume = 0f;
         audioSource.Play();
@@ -30,48 +35,74 @@ public class EnemyController : MonoBehaviour
         try
         {
             player = GameObject.FindGameObjectWithTag("Player");
+            enemyState = EnemyState.IDLE;
         }
         catch (System.Exception e)
         {
             //No object with the tag 'Player' was found
             Debug.Log(e.ToString() + "\nNo 'Player' object was found.  Did you forget to tag your player as 'Player'?");
+            enemyState = EnemyState.DEAD;
         }
 
     }
 
+
     // Update is called once per frame
     void Update()
     {
+        /*
+         * The Enemy cycles through a number of states: IDLE (out of range), ACTIVE 
+         * (in pursuit of player), DYING and DEAD.  We will assume that enemyFollowRange > audioRange.
+         */
+
         //calculate the distance to the player and set the volume accordingly
         float distance = Vector3.Distance(transform.position, player.transform.position);
 
-        if (player != null && distance <= ghostFollowRange)
+        switch (enemyState)
         {
-            //rotate ourself to face the player
-            transform.LookAt(player.transform);
+            //we are out of range
+            case EnemyState.IDLE:
+                if (distance <= enemyFollowRange) enemyState = EnemyState.ACTIVE;
+                break;
 
-            //move forward (towards the player) at the user specified speed
-            transform.Translate(Time.deltaTime * speed * transform.forward, Space.World);
+            case EnemyState.ACTIVE:
+                //rotate ourself to face the player
+                transform.LookAt(player.transform);
+
+                //move forward (towards the player) at the user specified speed
+                transform.Translate(Time.deltaTime * speed * transform.forward, Space.World);
+
+                if (distance < audioRange)
+                {
+                    //when the distance is 0, set the volume to 1 (max).  When the distance = audioRange, set the volume to 0 (min)
+                    audioSource.volume = (audioRange - distance) / audioRange;
+                }
+                else
+                {
+                    audioSource.volume = 0f;
+                }
+
+                //if the player runs too far away, go back to idleing
+                if (distance > enemyFollowRange) enemyState = EnemyState.IDLE;
+
+                break;
+
+            case EnemyState.DYING:
+                //destroy the object after the "zombie-hit" audio clip finishes
+                if (!audioSource.isPlaying)
+                {
+                    enemyState = EnemyState.DEAD;
+                }
+                break;
+
+            case EnemyState.DEAD:
+                //destroy the enemy
+                Destroy(gameObject);
+                break;
+
         }
 
-        //destroy the object after the "zombie-hit" audio clip finishes
-        if (bDestroy && !audioSource.isPlaying)
-        {
-            //destroy the enemy
-            Destroy(gameObject);
-        }
-        else
-        {
-            if (distance < audioRange)
-            {
-                //when the distance is 0, set the volume to 1 (max).  When the distance = audioRange, set the volume to 0 (min)
-                audioSource.volume = (audioRange - distance) / audioRange;
-            }
-            else
-            {
-                audioSource.volume = 0f;
-            }
-        }
+
     }
 
 
@@ -85,12 +116,12 @@ public class EnemyController : MonoBehaviour
         if (objectWeCollidedWith.tag == "Phaser")
         {
             //play the yell sound
-            audioSource.clip = ghostHitClip;
+            audioSource.clip = enemyHitClip;
             audioSource.loop = false;
             audioSource.Play();
 
             //mark ourselves for destruction when the audio clip ends
-            bDestroy = true;
+            enemyState = EnemyState.DYING;
 
         }
 
