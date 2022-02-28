47179c60e695f949b5bac5a2905e57a2fc0bf93e
jvolcy <jerry.volcy@gmail.com>
2022-02-28 10:13:06 -0500

    added collider

--- Assets/prefabs/Player/PlayerController.cs
+++ Assets/prefabs/Player/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
     {
 
     }
-
+    /*
     private void OnTriggerEnter(Collider other)
     {
         //get the GO associated with the thing we collided with.
@@ -39,7 +39,7 @@ public class PlayerController : MonoBehaviour
 
         if (objectWeCollidedWith.tag == "Enemy")
         {
-            //Debug.Log("ouch!");
+            Debug.Log("triggered!");
 
             //for now, destroy the enemy that touched us
             Destroy(objectWeCollidedWith);
@@ -51,12 +51,42 @@ public class PlayerController : MonoBehaviour
             Health.value -= 10;
         }
     }
+    */
+    private void OnCollisionEnter(Collision other)
+    {
+        //get the GO associated with the thing we collided with.
+        GameObject objectWeCollidedWith = other.gameObject;
 
-    void OnShoot()
+        //Debug.Log("ouch! " + objectWeCollidedWith.name);
+
+        if (objectWeCollidedWith.tag == "Enemy")
+        {
+            Debug.Log("collided!");
+
+            //for now, destroy the enemy that touched us
+            Destroy(objectWeCollidedWith);
+
+            //play the "ouch!" audio clip
+            audioSource.Play();
+
+            //decrease the health
+            Health.value -= 10;
+        }
+
+
+    }
+
+        void OnShoot()
     {
         Debug.Log("Shoot!");
         //instantiate a phaser 0.5 meters up from the floor and 2 meters ahead of the player.  This prevents us from shooting ourself!
         Instantiate(phaserPrefab, transform.position + 0.5f * transform.up + 2.0f * transform.forward, transform.rotation);
     }
 
+    void OnActivate()
+    {
+        Debug.Log("Activate!");
+        //instantiate a phaser 0.5 meters up from the floor and 2 meters ahead of the player.  This prevents us from shooting ourself!
+        Instantiate(phaserPrefab, transform.position + 0.5f * transform.up + 2.0f * transform.forward, transform.rotation);
+    }
 }
